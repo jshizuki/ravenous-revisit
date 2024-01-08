0056f7dd0824114295f1a434bbd07063d97ec147
Joyce Lau <jshizuki@yahoo.com.hk>
2024-01-08 23:03:42 +0900

    Further login logic with form

--- src/components/ButtonAppBar.js
+++ src/components/ButtonAppBar.js
@@ -9,10 +9,42 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import styles from "../css/ButtonAppBar.module.css";
+//
+import ReactModal from "react-modal";
 
 export default function ButtonAppBar({ toggleModal }) {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+
+  const toggleLoginForm = () => {
+    if (showLoginForm) {
+      setShowLoginForm(false);
+    } else {
+      setShowLoginForm(true);
+    };
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    if (username === "secret" && password === "secret") {
+      setIsLoggedIn(true);
+      setShowLoginForm(false);
+    } else {
+      setLoginError(true);
+    }
+  };
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -41,7 +73,33 @@ export default function ButtonAppBar({ toggleModal }) {
           <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
             <b>ravenous</b>
           </Typography>
-          <Button color="inherit">{isLoggedIn ? "logout" : "login"}</Button>
+          <Button color="inherit" onClick={toggleLoginForm}>
+            {isLoggedIn ? "logout" : "login"}
+          </Button>
+          {!isLoggedIn && (
+            <ReactModal
+              isOpen={showLoginForm}
+              onRequestClose={toggleLoginForm}
+              contentLabel="Login Form"
+            >
+              <form onSubmit={handleLoginSubmit}>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                {loginError && <p>Incorrect username or password</p>}
+                <button type="submit">Login</button>
+              </form>
+            </ReactModal>
+          )}
         </Toolbar>
       </AppBar>
       <Menu
