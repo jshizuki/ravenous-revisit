32231ec3d0336ded9370eec9acf67cc55a01f7df
Joyce Lau <jshizuki@yahoo.com.hk>
2024-01-12 16:47:33 +0900

    Added pagination for BusinessList

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Business from "./Business";
 // CSS styling
 import styles from "../css/BusinessList.module.css";
+// Material UI
+import Pagination from "@mui/material/Pagination";
 
 function BusinessList({ businesses, addReservation }) {
+  const businessesPerPage = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastBusiness = currentPage * businessesPerPage;
+  const indexOfFirstBusiness = indexOfLastBusiness - businessesPerPage;
+  const currentBusinesses = businesses.slice(
+    indexOfFirstBusiness,
+    indexOfLastBusiness
+  );
+
+  const handlePageChange = (event, pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div className={styles.allBusinessCardContainer}>
-      {businesses.map((business, index) => {
-        return <Business key={business.name + index} business={business} addReservation={addReservation} />;
-      })}
+    <div>
+      <Pagination
+        count={Math.ceil(businesses.length / businessesPerPage)}
+        page={currentPage}
+        onChange={handlePageChange}
+        className={styles.pagination}
+      />
+      <div className={styles.allBusinessCardContainer}>
+        {currentBusinesses.map((business, index) => {
+          return (
+            <Business
+              key={business.name + index}
+              business={business}
+              addReservation={addReservation}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
